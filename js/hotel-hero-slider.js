bc2db03a6c57e8ad69d2a33d47c1ced465f96caf
Vitalii1991 <ironwarrior1991@gmail.com>
2023-03-01 09:06:03 +0200

    1

---
+++ js/hotel-hero-slider.js
@@ -0,0 +1,53 @@
+let offset = 0;
+const photoSlider = document.querySelector(".hotel-photo-slider");
+const viewportWidth = document.body.clientWidth;
+const buttonLeft = document.querySelector(".hotel-btn-left");
+const buttonRight = document.querySelector(".hotel-btn-right");
+
+if (viewportWidth >= 1440) {
+  buttonRight.addEventListener("click", function () {
+    offset = offset + 468;
+    if (offset > 936) {
+      offset = 936;
+    }
+    photoSlider.style.right = offset + "px";
+  });
+
+  buttonLeft.addEventListener("click", function () {
+    offset = offset - 468;
+    if (offset < 0) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + "px";
+  });
+} else if (viewportWidth >= 768) {
+  buttonRight.addEventListener("click", function () {
+    offset = offset + 468;
+    if (offset > 936) {
+      offset = 936;
+    }
+    photoSlider.style.right = offset + "px";
+  });
+  buttonLeft.addEventListener("click", function () {
+    offset = offset - 468;
+    if (offset < 0) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + "px";
+  });
+} else {
+  buttonRight.addEventListener("click", function () {
+    offset = offset + 270;
+    if (offset > 540) {
+      offset = 540;
+    }
+    photoSlider.style.right = offset + "px";
+  });
+  buttonLeft.addEventListener("click", function () {
+    offset = offset - 270;
+    if (offset < 0) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + "px";
+  });
+}
